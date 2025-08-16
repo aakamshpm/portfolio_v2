614d91c4bfc83a6acee8b13fb9318f896d78987d
Aakamsh PM <aakamshpm@gmail.com>
2025-08-16 22:00:50 +0530

    feat: update ProjectCard component to accept ProjectData props and apply styling

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -1,5 +1,7 @@
-const ProjectCard = () => {
-  return <div>ProjectCard</div>;
+import type { ProjectData } from "../interface";
+
+const ProjectCard = ({}: ProjectData) => {
+  return <div className="border border-black rounded-4xl p-4"></div>;
 };
 
 export default ProjectCard;
